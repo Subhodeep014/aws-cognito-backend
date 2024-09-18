96ec21702cdbe22b172e48ae9340666bcea5afea
Subhodeep <subhodeepbasak14@gmail.com>
2024-09-19 00:08:13 +0530

    chnahges

--- api/index.js
+++ api/index.js
@@ -17,11 +17,11 @@ app.use('/api/user', authRouter)
 app.use('/api/todo', todoRouter)
 
 
-app.use(express.static(path.join(_dirname, '/client/dist')));
+// app.use(express.static(path.join(_dirname, '/client/dist')));
 
-app.get('*', (req, res)=>{
-  res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'))
-})
+// app.get('*', (req, res)=>{
+//   res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'))
+// })
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal server error';
